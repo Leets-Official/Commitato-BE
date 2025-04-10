051aff25e53267e16e0f016f0eb597a10a27438d
1winhyun <jimking1@naver.com>
2025-04-10 10:03:57 +0900

    refactor: ExpAndTierTestController 앤드포인트 수정

--- src/main/java/com/leets/commitatobe/domain/commit/controller/ExpAndTierTestController.java
+++ src/main/java/com/leets/commitatobe/domain/commit/controller/ExpAndTierTestController.java
@@ -16,14 +16,14 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "Exp, Tier 업데이트 컨트롤러", description = "입력한 Exp에 따른 사용자의 Exp와 Tier 데이터 가공을 처리합니다.")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("user/update")
 public class ExpAndTierTestController {
 	private final ExpService expService;
 
 	@Operation(
 		summary = "경험치 및 티어 업데이트",
 		description = "테스트를 위해, 사용자가 경험치 입력 시 경험치와 티어를 해당 경험치에 맞게 업데이트합니다.")
-	@PatchMapping("update/expAndTier")
+	@PatchMapping("/exp-tier")
 	public ApiResponse<ExpAndTierResponse> updateExpAndTier(@RequestParam("exp") int exp) {
 		return ApiResponse.onSuccess(expService.updateExpAndTier(exp));
 	}
