b34f358e0889be401e873953810fe31cbd62bebc
1winhyun <jimking1@naver.com>
2025-05-26 15:44:22 +0900

    feat: 랭킹 스케줄러 구현

---
+++ src/main/java/com/leets/commitatobe/domain/user/service/RankingScheduler.java
@@ -0,0 +1,37 @@
+package com.leets.commitatobe.domain.user.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.leets.commitatobe.domain.user.domain.User;
+import com.leets.commitatobe.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class RankingScheduler {
+	private final UserRepository userRepository;
+
+	@Scheduled(cron = "0 41 0/3 * * *")
+	@Transactional
+	public void updateUserRankings() {
+		List<User> allUsers = userRepository.findAllByOrderByExpDesc(Pageable.unpaged()).getContent();
+
+		int ranking = 0;
+		int previousExp = -1;
+
+		for (User user : allUsers) {
+			if (!user.getExp().equals(previousExp)) {
+				ranking++;
+				previousExp = user.getExp();
+			}
+			user.updateRank(ranking);
+			userRepository.save(user);
+		}
+	}
+}
