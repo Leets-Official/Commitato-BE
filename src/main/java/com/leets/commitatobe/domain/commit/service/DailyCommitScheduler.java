18b75825a8874b67bad25942cccec75c8d1282e9
1winhyun <jimking1@naver.com>
2025-07-26 16:57:13 +0900

    refactor: 스케줄러 작동 시간 변경

--- src/main/java/com/leets/commitatobe/domain/commit/service/DailyCommitScheduler.java
+++ src/main/java/com/leets/commitatobe/domain/commit/service/DailyCommitScheduler.java
@@ -24,7 +24,7 @@ public class DailyCommitScheduler {
 	private final ExpService expService;
 	private final AuthService authService;
 
-	@Scheduled(cron = "0 51 16 * * *")
+	@Scheduled(cron = "0 30 06 * * *")
 	@Transactional
 	public void updateAllUsersCommits() {
 		List<User> users = userRepository.findAll();
