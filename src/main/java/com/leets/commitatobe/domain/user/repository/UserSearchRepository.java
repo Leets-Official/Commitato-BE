b87a169587b40d8132bd53d8532837aa4e52a381
1winhyun <jimking1@naver.com>
2025-08-03 23:06:53 +0900

    refactor: UserSearchRepository 필요없는 부분 삭제

--- src/main/java/com/leets/commitatobe/domain/user/repository/UserSearchRepository.java
+++ src/main/java/com/leets/commitatobe/domain/user/repository/UserSearchRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import com.leets.commitatobe.domain.user.domain.UserDocument;
 
 public interface UserSearchRepository extends ElasticsearchRepository<UserDocument, String> {
-	List<UserDocument> findByGithubIdStartingWith(String githubId);
-
 	List<UserDocument> findByGithubIdContaining(String githubId);
 }
