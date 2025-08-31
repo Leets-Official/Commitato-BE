7e0214fe56c55e26e8314f04fb51210c73a0c2b2
1winhyun <jimking1@naver.com>
2025-08-31 22:50:59 +0900

    refactor: GithubTokenRedis Id관련 임포트문 수정

--- src/main/java/com/leets/commitatobe/domain/token/entity/GithubTokenRedis.java
+++ src/main/java/com/leets/commitatobe/domain/token/entity/GithubTokenRedis.java
@@ -1,8 +1,8 @@
 package com.leets.commitatobe.domain.token.entity;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
